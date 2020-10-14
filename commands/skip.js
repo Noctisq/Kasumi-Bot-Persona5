5220579895c073671fe003db219210b78cc0bb55
Noctisq <chrismichele@hotmail.com>
2020-10-14 12:54:21 -0500

    More fixes

--- commands/skip.js
+++ commands/skip.js
@@ -7,10 +7,9 @@ module.exports = {
         return message.channel.send(
           "No estás en ningún canal de voz, senpai :c"
         );
-      const voiceChannel = await bot.voice.connections.first(1);
-
-      const dispatcher = voiceChannel[0].dispatcher;
-      dispatcher.emit("finish");
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.dispatcher;
+      dispatcher.emit('finish');
     } else {
       message.reply(
         "Únete a un canal de voz primero y después ejecuta el mismo comando :heart:"
